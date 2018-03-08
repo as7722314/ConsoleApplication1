e1d9f8e54b460d6f1c4b70bb36e5a896b57694e6
user <user@MA324-46>
2018-03-08 15:00:07 +0800

    add dog and add tiger

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("cat");
+            Console.WriteLine("dog");
+            Console.WriteLine("tiger");
         }
     }
 }
